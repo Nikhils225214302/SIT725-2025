3954c9cfd140a4828309644a28babfd65a1249e4
Nikhil <niki.ranjit@gmail.com>
2025-07-31 10:17:07 +1000

    This is week-3

--- week-3/server.js
+++ week-3/server.js
@@ -1,9 +1,9 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+app.listen(port, () => {
+  console.log("App listening to: " + port);
+});
